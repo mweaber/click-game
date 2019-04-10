891982a097bf5839164721d07756661678b7577e
Matthew Weaber <matthewweaber89@gmail.com>
2019-04-09 21:50:30 -0400

    Updates

---
+++ precache-manifest.154e05206e8b71ca7eddfb473d398b80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  },
+  {
+    "revision": "74eadeaec0e35b57b2a0",
+    "url": "/click-game/static/js/main.c39dcc24.chunk.js"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/click-game/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "74eadeaec0e35b57b2a0",
+    "url": "/click-game/static/css/main.b0786d6c.chunk.css"
+  },
+  {
+    "revision": "666cddd4e414cd6cd7abf9e7e7b39302",
+    "url": "/click-game/index.html"
+  }
+];
